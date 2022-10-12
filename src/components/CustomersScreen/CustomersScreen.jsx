0e3eec217a187c7138b3a32afe16f712e4673903
Rohit Rajendra Barate <rohitbarate100@gmail.com>
2022-10-12 23:46:28 +0530

    code refactor

--- src/components/CustomersScreen/CustomersScreen.jsx
+++ src/components/CustomersScreen/CustomersScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CustomerRow from "../CustomerRow/CustomerRow";
 import "./CustomerScreen.css";
 import customerData from "../../DATA.json";
